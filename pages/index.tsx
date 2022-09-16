10fb099f7db2e1001d4a43e3d4059187d9ac725c
Penguinz22 <nate19522@gmail.com>
2022-09-16 14:10:35 -0700

    Added SpotTag project

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,9 +49,9 @@ const Home: NextPage = () => {
           <p className="text-gray-400 text-xl mb-5 lg:w-2/3 md:w-full font-thin ld:text-left text-justify">
             {"Hey my name is Nathan and I'm looking to pursue a software developer position in the future and I'm striving to achieve that. I began interested in coding when I learned about Minecraft plugins. Through that time I learned Java slowly and later became interested in graphics with OpenGL and over the years have tried developing multiple small game engines."}
             <br /><br />
-            {"Most recently I've worked on a game engine named "}<LinkedText to="#projects">kotaro</LinkedText>{" developed in Kotlin. Right now I'm working on a software as a service webapp that manages fitness studio requirements such as memberships, billing, students, and more. This project is being developed in Typescript and leads me to an interest in full-stack development."}
+            {"Most recently I've worked on a game engine named "}<LinkedText to="#projects">kotaro</LinkedText>{" developed in Kotlin. I've always been passionate about creating full experiences on my own such as the previously mentioned game engine. All key systems in there were written with minimal use of libraries and utilized different sub projects together such as the editor and engine. I also enjoy working on full stack applications, that even though don't get fully finished and polished, provide me a learning experience of managing a project with multiple components that work together while minimizing issues."}
             <br /><br />
-            {"Currently I'm pursuing an education in computer science at ASU. I'm also seeking internship opportunities during the summer to build my skills and prepare for my future."}
+            {"Currently I'm pursuing an education in computer science at ASU with a graduation year of 2026. I'm also seeking internship opportunities during the summer to build my skills and prepare for my future."}
           </p>
         </div>
       </section>
@@ -92,6 +92,22 @@ const Home: NextPage = () => {
             </div>
             <div className="xl:relative xl:left-0 xl:min-h-full xl:w-7/12 md:absolute lg:left-[-7rem] md:left-[-3rem] min-h-[125%] md:min-h-[115%] lg:mt-5 lg:w-9/12 md:w-8/12 md:mt-16 mt-5 relative w-full">
               <Image src="/pengo-example.png" className="z-0" alt="" layout="responsive" width="100%" height="56.25%"/>
+            </div>
+          </section>
+          <section className="w-full flex md:flex-row-reverse flex-col mt-20 transition-all duration-500 delay-300 fade-in">
+            <div className = "overflow-visible z-50 mt-10 md:w-5/12 w-full flex flex-col md:block">
+              <div className="text-lg text-palette-1 flex-col font-thin md:text-right text-left">Spotify Client<h2 className="text-gray-300 text-4xl font-semibold">SpotTag</h2></div>
+              <div className="flex-grow">
+                <div className="mt-4 p-4 bg-slate-800 rounded-md lg:w-[120%] md:w-[140%] w-full z-50 overflow-visible pb-2 md:float-right">
+                  <p className="text-gray-200 text-lg mb-5">A spotify client built for the purpose of collaborative listening sessions and also adding extra tags to songs to enable creating playlists based off of mood, color, or emojis. This project is currently under development but is being worked on open source.</p>
+                </div>
+                <div className="mt-4 max-w-[2.25rem] md:float-right">
+                  <a href="https://github.com/Pengiie/spottag" target="_blank" rel="noreferrer" className="w-10"><AiFillGithub className="text-white hover:text-palette-1 h-9 w-9" /></a>
+                </div>
+              </div>
+            </div>
+            <div className="xl:relative xl:left-0 xl:min-h-full xl:w-7/12 md:absolute lg:left-[-7rem] md:left-[-3rem] min-h-[125%] md:min-h-[115%] lg:mt-5 lg:w-9/12 md:w-8/12 md:mt-16 mt-5 relative w-full">
+              <Image src="/spottag-example.png" className="z-0" alt="" layout="responsive" width="100%" height="56.25%"/>
             </div>
           </section>
         </div>
